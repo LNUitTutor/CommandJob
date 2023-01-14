b3ebf6d5797ba7e36113ee7f9243bdfdf56780b6
Serhiy Yaroshko <lnu.it.tutor@gmail.com>
2023-01-14 19:24:12 +0200

    Comment Functions.cpp

--- CommandJob/Functions.cpp
+++ CommandJob/Functions.cpp
@@ -1,10 +1,12 @@
 #include "Functions.h"
 
+// function take two values of numbers and return its summa
 double add(double x, double y)
 {
 	return x + y;
 }
 
+// function take two values of numbers and return its product
 double mult(double x, double y)
 {
 	return x * y;
